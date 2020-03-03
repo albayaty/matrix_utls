e7368768a047ffc00cab3968cbf82cc39f718327
Ali Al-Bayaty <albayaty@users.noreply.github.com>
2020-03-03 04:41:14 -0800

    Update pack.pl

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(matrix_utls).
 title('Matrix utilities: Kronecker (Tensor), Hadamard, and matrices creation/multiplications').
-version('1.0').
+version('1.1').
 author('Ali Al-Bayaty', 'albayaty@pdx.edu').
 author('Marek Perkowski', 'h8mp@pdx.edu').
 maintainer('Ali Al-Bayaty', 'aalba3@unh.newhaven.edu').
