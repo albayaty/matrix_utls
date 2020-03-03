817701455942e8901c7151bf1c47834c4eb4b8f2
Ali Al-Bayaty <albayaty@users.noreply.github.com>
2020-03-02 19:15:14 -0800

    Add files via upload

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(matrix_utls).
+title('Matrix utilities: Kronecker (Tensor), Hadamard, and matrices creation/multiplications').
+version('1.0').
+author('Ali Al-Bayaty', 'albayaty@pdx.edu').
+author('Marek Perkowski', 'h8mp@pdx.edu').
+maintainer('Ali Al-Bayaty', 'aalba3@unh.newhaven.edu').
+home('https://github.com/albayaty/matrix_utls.git').
+download('https://github.com/albayaty/matrix_utls/blob/master/releases/v1.0.zip').
